d77486f871d05e0bd73625750ddf823c25a70f15
Divyesh Trivedi <dt7215230@gmail.com>
2025-03-28 13:33:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer.jsx";
+import {useRef} from "react";
+
+function App() {
+    const webGiViewerRef = useRef();
+
+    const contentRef = useRef();
+    const handlePreview = () => {
+        webGiViewerRef.current.triggerPreview();
+    }
+  return (
+    <div className="App">
+        <div ref={contentRef} id="content">
+            <Nav />
+            <Jumbotron />
+            <SoundSection />
+            <DisplaySection triggerPreview={handlePreview} />
+        </div>
+      <WebgiViewer contentRef={contentRef} ref={webGiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
